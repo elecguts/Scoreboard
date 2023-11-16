8cccd31ac7a171a884dd60316e4bfa57031c25b1
Samuel Gonzalez <Nomekop65@yahoo.com>
2023-11-15 20:15:25 -0500

    Experimental event listeners added for testing

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-import "./style.css";
+import './style.css'
+
+function displayMatches() {
+  console.log("Some button with the class 'add' was clicked")
+}
+const addButton = document.querySelector('section.team1 .add')
+addButton?.addEventListener('click', displayMatches)
+
+const addButton2 = document.querySelector('section.team2 .add')
+addButton2?.addEventListener('click', displayMatches)
