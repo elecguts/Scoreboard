d617c1b035ad8ebd49a64b4c8138dde050f5a91c
Samuel Gonzalez <Nomekop65@yahoo.com>
2023-12-05 19:58:07 -0500

    Plus and minus buttons work for each team to increment and decrement h3 scores

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,41 @@
 import './style.css'
 
-function displayMatches() {
-  console.log("Some button with the class 'add' was clicked")
-}
-const addButton = document.querySelector('section.team1 .add')
-addButton?.addEventListener('click', displayMatches)
+const teamOneScoreContents = document.querySelector('section.team1 h3')
+let teamOneScore = 0
+const teamTwoScoreContents = document.querySelector('section.team2 h3')
+let teamTwoScore = 0
 
+const addButton = document.querySelector('section.team1 .add')
 const addButton2 = document.querySelector('section.team2 .add')
-addButton2?.addEventListener('click', displayMatches)
+const minusButton = document.querySelector('section.team1 .subtract')
+const minusButton2 = document.querySelector('section.team2 .subtract')
+
+function clickOnTeamOnePlusButton() {
+  teamOneScore = teamOneScore + 1
+  if (teamOneScoreContents) {
+    teamOneScoreContents.textContent = `${teamOneScore}`
+  }
+}
+function clickOnTeamTwoPlusButton() {
+  teamTwoScore = teamTwoScore + 1
+  if (teamTwoScoreContents) {
+    teamTwoScoreContents.textContent = `${teamTwoScore}`
+  }
+}
+function clickOnTeamOneMinusButton() {
+  teamOneScore = teamOneScore - 1
+  if (teamOneScoreContents) {
+    teamOneScoreContents.textContent = `${teamOneScore}`
+  }
+}
+function clickOnTeamTwoMinusButton() {
+  teamTwoScore = teamTwoScore - 1
+  if (teamTwoScoreContents) {
+    teamTwoScoreContents.textContent = `${teamTwoScore}`
+  }
+}
+
+addButton?.addEventListener('click', clickOnTeamOnePlusButton)
+addButton2?.addEventListener('click', clickOnTeamTwoPlusButton)
+minusButton?.addEventListener('click', clickOnTeamOneMinusButton)
+minusButton2?.addEventListener('click', clickOnTeamTwoMinusButton)
