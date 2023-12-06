0379f1fc5bff69118f585c6eacec5be4ab4f8576
Samuel Gonzalez <Nomekop65@yahoo.com>
2023-12-05 20:54:28 -0500

    Added guard clauses to prevent either score from going below zero or above 21

--- src/main.ts
+++ src/main.ts
@@ -11,24 +11,36 @@ const minusButton = document.querySelector('section.team1 .subtract')
 const minusButton2 = document.querySelector('section.team2 .subtract')
 
 function clickOnTeamOnePlusButton() {
+  if (teamOneScore === 21) {
+    return
+  }
   teamOneScore = teamOneScore + 1
   if (teamOneScoreContents) {
     teamOneScoreContents.textContent = `${teamOneScore}`
   }
 }
 function clickOnTeamTwoPlusButton() {
+  if (teamTwoScore === 21) {
+    return
+  }
   teamTwoScore = teamTwoScore + 1
   if (teamTwoScoreContents) {
     teamTwoScoreContents.textContent = `${teamTwoScore}`
   }
 }
 function clickOnTeamOneMinusButton() {
+  if (teamOneScore === 0) {
+    return
+  }
   teamOneScore = teamOneScore - 1
   if (teamOneScoreContents) {
     teamOneScoreContents.textContent = `${teamOneScore}`
   }
 }
 function clickOnTeamTwoMinusButton() {
+  if (teamTwoScore === 0) {
+    return
+  }
   teamTwoScore = teamTwoScore - 1
   if (teamTwoScoreContents) {
     teamTwoScoreContents.textContent = `${teamTwoScore}`
