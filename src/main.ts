28f7a5b3080354f79be4ad8c46b5123638f3b5d6
Samuel Gonzalez <Nomekop65@yahoo.com>
2023-12-19 20:01:31 -0500

    Team names now match input fields

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,15 @@ const addButton2 = document.querySelector('section.team2 .add')
 const minusButton = document.querySelector('section.team1 .subtract')
 const minusButton2 = document.querySelector('section.team2 .subtract')
 
+const teamOneNameInput = document.querySelector('section.team1 input')
+const teamTwoNameInput = document.querySelector('section.team2 input')
+console.log(teamOneNameInput)
+console.log(teamTwoNameInput)
+const teamOneNameText = document.querySelector('section.team1 h2')
+const teamTwoNameText = document.querySelector('section.team2 h2')
+console.log(teamOneNameText)
+console.log(teamTwoNameText)
+
 function clickOnTeamOnePlusButton() {
   if (teamOneScore === 21) {
     return
@@ -46,8 +55,31 @@ function clickOnTeamTwoMinusButton() {
     teamTwoScoreContents.textContent = `${teamTwoScore}`
   }
 }
+function teamOneInputChanged(event: any) {
+  const inputThatWasChanged = event.target
 
+  if (inputThatWasChanged instanceof HTMLInputElement) {
+    const textThatWasInput = inputThatWasChanged.value
+
+    if (teamOneNameText) {
+      teamOneNameText.textContent = textThatWasInput
+    }
+  }
+}
+function teamTwoInputChanged(event: any) {
+  const inputThatWasChanged = event.target
+
+  if (inputThatWasChanged instanceof HTMLInputElement) {
+    const textThatWasInput = inputThatWasChanged.value
+
+    if (teamTwoNameText) {
+      teamTwoNameText.textContent = textThatWasInput
+    }
+  }
+}
 addButton?.addEventListener('click', clickOnTeamOnePlusButton)
 addButton2?.addEventListener('click', clickOnTeamTwoPlusButton)
 minusButton?.addEventListener('click', clickOnTeamOneMinusButton)
 minusButton2?.addEventListener('click', clickOnTeamTwoMinusButton)
+teamOneNameInput?.addEventListener('input', teamOneInputChanged)
+teamTwoNameInput?.addEventListener('input', teamTwoInputChanged)
